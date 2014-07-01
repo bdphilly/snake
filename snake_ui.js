a5b293c4da36d5159579460fd271602a45109006
Bryan Phillips <bdphilly@gmail.com>
2014-06-30 18:34:04 -0700

    fix double move bug

--- snake_ui.js
+++ snake_ui.js
@@ -7,44 +7,56 @@
   }
 
   View.prototype.handleKeyEvent = function (event) {
-    switch(event.keyCode) {
-      case 87:
-      case 38:
-        if (this.snake.direction !== "S") {
-          this.snake.turn("N");
-        }
-        break;
-      case 68:
-      case 39:
-        if (this.snake.direction !== "W") {
-          this.snake.turn("E");
-        }
-        break;
-      case 83:
-      case 40:
-        if (this.snake.direction !== "N") {
-          this.snake.turn("S");
-        }
-        break;
-      case 65:
-      case 37:
-        if (this.snake.direction !== "E") {
-          this.snake.turn("W");
-        }
-        break;
-      default:
-        break;
+    if (this.moveFlag === false) {
+      switch(event.keyCode) {
+        case 87:
+        case 38:
+          if (this.snake.direction !== "S") {
+            this.snake.turn("N");
+            this.moveFlag = true;
+          }
+          break;
+        case 68:
+        case 39:
+          if (this.snake.direction !== "W") {
+            this.snake.turn("E");
+            this.moveFlag = true;
+          }
+          break;
+        case 83:
+        case 40:
+          if (this.snake.direction !== "N") {
+            this.snake.turn("S");
+            this.moveFlag = true;            
+          }
+          break;
+        case 65:
+        case 37:
+          if (this.snake.direction !== "E") {
+            this.snake.turn("W");
+            this.moveFlag = true;            
+          }
+          break;
+        default:
+          break;
+      }
     }
   };
 
+  View.prototype.resetFlag = function () {
+    this.moveFlag = false;
+  }
+
+
+ 
   View.prototype.start = function () {
     this.snake = new SnakeGame.Snake();
     var view = this
 
-    function step () {
-      this.snake.render();
-      this.snake.move();
-    }
+    // function step () {
+    //   this.snake.render();
+    //   this.snake.move();
+    // }
 
     $(document).ready(function() {
       $(document).keydown(function(event) {
@@ -55,6 +67,7 @@
     var timerId = 0;
     
     timerId = setInterval(function () {
+      view.resetFlag();
       view.snake.render();
       view.snake.move();
       if (view.snake.isDead) {
